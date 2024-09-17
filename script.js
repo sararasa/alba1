a092064826fcff7b67a8f97324f9db23a156cc3c
sararasa <169053894+sararasa@users.noreply.github.com>
2024-09-17 10:56:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector(".rect-button").addEventListener("click", function() {
+    // Creare un nuovo elemento di testo
+    const newText = document.createElement("div");
+    newText.classList.add("additional-text");
+    newText.innerHTML = `<p>Unt venimolupta quas unt auditatur sam faccumque sum eri quam quis erum fuga. Aque nonsequid eosam, iditas endaerum con rehendit moluptat et omnia dest res doluptia doloressimus arum exerrum facea volupta aliquis se placi sum quiandi dolutassum rerum aut autem suntiatenem. Et et aut millique re sed minciatet reptatatquam quidell esedit repra a suntibus et volore con nostrum, officae pro officia tempore heneces cipsandit et as nos a simos sernat.
+Modis dolorrovit ipient lat.</p>`;
+    
+const gifSection = document.querySelector(".gif");
+gifSection.appendChild(newText);
+
+    // Modificare lo stile del footer per farlo scalare
+    const footer = document.querySelector(".footer");
+    footer.style.position = "relative";
+    footer.style.top = "auto";
+
+    // Far scomparire il pulsante dopo il clic
+    this.style.display = "none";
+});
